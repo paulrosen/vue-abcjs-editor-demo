7b7d596f069f67e88b38c1e6574059b59ca07781
Paul Rosen <paul@paulrosen.net>
2017-04-24 21:42:12 -0400

    Demo for using a textarea to edit music on a webpage.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # vue-abcjs-editor-demo
-Simplest demo for using ABC notation in a textarea on a website.
+> Simplest demo for using ABC notation in a textarea on a website.
+
+See [abcjs](https://github.com/paulrosen/abcjs) for details.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/components/Hello.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="hello">
+		<h1>Vue Editor abcjs Demo</h1>
+		<p>This is a simple app that uses a textarea to display some sheet music.</p>
+		<p>The only addition to the standard vue-cli app's dependencies is:</p>
+		<pre>npm install abcjs --save</pre>
+		<p>The only necessary code to make this work is:</p>
+		<pre>import abcjs from "abcjs";
+new abcjs.Editor("abc-source",
+    {canvas_id: "paper"});</pre>
+		<textarea id="abc-source">{{tune}}</textarea>
+		<div id="midi"></div>
+		<div id="paper"></div>
+	</div>
+</template>
+
+<script>
+	import abcjs from "abcjs";
+
+	export default {
+		mounted: function () {
+			new abcjs.Editor("abc-source", {canvas_id: "paper"});
+		},
+		name: 'hello',
+		data () {
+			return {
+				tune: 'X:1\nT: Cooley\'s\nM: 4/4\nL: 1/8\nR: reel\nK: Emin\nD2|:"Em"EB{c}BA B2 EB|~B2 AB dBAG|"D"FDAD BDAD|FDAD dAFD|\n"Em"EBBA B2 EB|B2 AB defg|"D"afe^c dBAF|1"Em"DEFD E2 D2:|2"Em"DEFD E2 gf||\n|:"Em"eB B2 efge|eB B2 gedB|"D"A2 FA DAFA|A2 FA defg|\n"Em"eB B2 eBgB|eB B2 defg|"D"afe^c dBAF|1"Em"DEFD E2 gf:|2"Em"DEFD E4|]\n'
+			}
+		}
+	}
+</script>
+
+<style>
+	#abc-source {
+		width: 350px;
+		height: 300px;
+	}
+
+	pre {
+		border: 1px solid #888888;
+		padding: 6px;
+		border-radius: 4px;
+		width: 350px;
+	}
+
+</style>
